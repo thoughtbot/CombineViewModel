794fb579ce1f2719823c1f4c551ee2ae80aa0fd4
Adam Sharp <adam@sharplet.me>
2019-08-31 14:41:05 -0400

    Add ~> operator for output bindings

--- Bindings/BindingSubscriber.swift
+++ Bindings/BindingSubscriber.swift
@@ -1,8 +1,23 @@
 import Combine
 
 infix operator <~
+infix operator ~>
 
 public protocol BindingSubscriber: Subscriber, Cancellable {
+  @discardableResult
   static func <~ <P: Publisher> (subscriber: Self, source: P) -> AnyCancellable
     where P.Output == Input, P.Failure == Failure
+
+  @discardableResult
+  static func ~> <P: Publisher> (source: P, subscriber: Self) -> AnyCancellable
+    where P.Output == Input, P.Failure == Failure
+}
+
+extension BindingSubscriber {
+  @discardableResult
+  public static func ~> <P: Publisher> (source: P, subscriber: Self) -> AnyCancellable
+    where P.Output == Input, P.Failure == Failure
+  {
+    subscriber <~ source
+  }
 }
