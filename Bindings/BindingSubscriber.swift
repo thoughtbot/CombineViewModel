74b0ef72941f59f48bf184d61ec68d5fb5f3a121
Adam Sharp <adam@sharplet.me>
2019-08-31 14:54:19 -0400

    Fix precedence for binding output operator

--- Bindings/BindingSubscriber.swift
+++ Bindings/BindingSubscriber.swift
@@ -1,22 +1,22 @@
 import Combine
 
-infix operator <~
-infix operator ~>
+precedencegroup BindingPrecedence {
+  higherThan: AssignmentPrecedence
+  assignment: true
+}
+
+infix operator <~: BindingPrecedence
 
 public protocol BindingSubscriber: Subscriber, Cancellable {
   @discardableResult
   static func <~ <P: Publisher> (subscriber: Self, source: P) -> AnyCancellable
     where P.Output == Input, P.Failure == Failure
-
-  @discardableResult
-  static func ~> <P: Publisher> (source: P, subscriber: Self) -> AnyCancellable
-    where P.Output == Input, P.Failure == Failure
 }
 
-extension BindingSubscriber {
+extension Publisher {
   @discardableResult
-  public static func ~> <P: Publisher> (source: P, subscriber: Self) -> AnyCancellable
-    where P.Output == Input, P.Failure == Failure
+  public static func ~> <B: BindingSubscriber> (source: Self, subscriber: B) -> AnyCancellable
+    where Output == B.Input, Failure == B.Failure
   {
     subscriber <~ source
   }
