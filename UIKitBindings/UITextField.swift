8ea25984c36c4606106374ce21e0e5399d6d564d
Adam Sharp <adam@sharplet.me>
2019-09-24 14:40:39 -0400

    Add UITextField text bindings

--- UIKitBindings/UITextField.swift
+++ UIKitBindings/UITextField.swift
@@ -2,6 +2,14 @@ import Bindings
 import UIKit
 
 extension Reactive where Base: UITextField {
+  public var text: BindingSink<Base, String?> {
+    BindingSink(owner: base) { $0.text = $1 }
+  }
+
+  public var attributedText: BindingSink<Base, NSAttributedString?> {
+    BindingSink(owner: base) { $0.attributedText = $1 }
+  }
+
   public var placeholder: BindingSink<Base, String?> {
     BindingSink(owner: base) { $0.placeholder = $1 }
   }
