bf183c544c32540106ca65625004e381bc05ecb1
Adam Sharp <adam@sharplet.me>
2020-08-01 00:04:23 -0400

    Persist task list

--- Example/Sources/TaskList.swift
+++ Example/Sources/TaskList.swift
@@ -1,13 +1,17 @@
 import Combine
 import Foundation
 
-struct Task: Hashable {
+struct Task: Hashable, Codable {
   var id: UUID
   var title: String
 }
 
 final class TaskList: ObservableObject {
-  @Published private(set) var tasks: [Task] = []
+  @Published private(set) var tasks: [Task]
+
+  init(tasks: [Task] = []) {
+    self.tasks = tasks
+  }
 
   func appendNew(title: String) {
     let task = Task(id: UUID(), title: title)
--- Example/Sources/TasksViewController.swift
+++ Example/Sources/TasksViewController.swift
@@ -1,6 +1,7 @@
 import Combine
 import CombineViewModel
 import UIKit
+import os.log
 
 final class TasksViewController: UITableViewController, ViewModelObserver {
   private enum Section {
@@ -8,13 +9,26 @@ final class TasksViewController: UITableViewController, ViewModelObserver {
   }
 
   private var dataSource: UITableViewDiffableDataSource<Section, Task>!
+  private let decoder = JSONDecoder()
+  private let encoder = JSONEncoder()
+  private let url = try! FileManager.default
+    .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    .appendingPathComponent("tasks.json", isDirectory: false)
   @ViewModel private var taskList: TaskList
 
   var subscriptions: Set<AnyCancellable> = []
 
   required init?(coder: NSCoder) {
     super.init(coder: coder)
-    taskList = TaskList()
+
+    do {
+      let data = try Data(contentsOf: url)
+      let tasks = try decoder.decode([Task].self, from: data)
+      taskList = TaskList(tasks: tasks)
+    } catch {
+      os_log(.error, "Error loading task list: %@", "\(error)")
+      taskList = TaskList()
+    }
   }
 
   override func viewDidLoad() {
@@ -25,6 +39,8 @@ final class TasksViewController: UITableViewController, ViewModelObserver {
       cell.textLabel?.text = item.title
       return cell
     }
+
+    updateView()
   }
 
   func updateView() {
@@ -32,6 +48,13 @@ final class TasksViewController: UITableViewController, ViewModelObserver {
     snapshot.appendSections([.tasks])
     snapshot.appendItems(taskList.tasks)
     dataSource.apply(snapshot)
+
+    do {
+      let data = try encoder.encode(taskList.tasks)
+      try data.write(to: url)
+    } catch {
+      os_log(.error, "Error saving task list: %@", "\(error)")
+    }
   }
 
   @IBAction func newTask(_ sender: Any?) {
