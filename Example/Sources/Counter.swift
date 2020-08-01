6f2fba91cf522c01718c4c7fc371450d3efea183
Adam Sharp <adam@sharplet.me>
2020-07-31 23:03:38 -0400

    Add example app

---
+++ Example/Sources/Counter.swift
@@ -0,0 +1,27 @@
+import Combine
+import Foundation
+
+final class Counter: ObservableObject {
+  let defaultValue: Int
+  @Published var value: Int
+
+  init(value: Int, defaultValue: Int) {
+    self.defaultValue = defaultValue
+    self.value = value
+  }
+
+  var formattedValue: String {
+    NumberFormatter.localizedString(
+      from: NSNumber(value: value),
+      number: .decimal
+    )
+  }
+
+  var isDefault: Bool {
+    value == defaultValue
+  }
+
+  func reset() {
+    value = defaultValue
+  }
+}
---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,40 @@
+import Combine
+import CombineViewModel
+import UIKit
+
+final class ViewController: UITableViewController, ViewModelObserver {
+  @IBOutlet private var resetItem: UIBarButtonItem!
+  @IBOutlet private var stepper: UIStepper!
+  @IBOutlet private var valueLabel: UILabel!
+
+  @ViewModel private var counter: Counter
+
+  var subscriptions: Set<AnyCancellable> = []
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    counter = Counter(
+      value: UserDefaults.standard.integer(forKey: "counterValue"),
+      defaultValue: 0
+    )
+    stepper.maximumValue = .infinity
+    stepper.minimumValue = -.infinity
+  }
+
+  func updateView() {
+    resetItem.isEnabled = !counter.isDefault
+    stepper.value = Double(counter.value)
+    valueLabel.text = counter.formattedValue
+    UserDefaults.standard.set(counter.value, forKey: "counterValue")
+  }
+
+  @IBAction func reset(_ sender: Any?) {
+    counter.reset()
+  }
+
+  @IBAction func step(_ stepper: UIStepper) {
+    counter.value = Int(stepper.value)
+  }
+}
+
